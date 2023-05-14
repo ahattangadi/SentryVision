40c2a9d38c16bd48f1a8368c5144b8bab4dbac92
Aarav Hattangadi <aarav@hattangadi.com>
2023-05-15 01:08:55 +0530

    Added token validation
    - Added endpoint (POST) which checks against the database for valid tokens
    - The POST endpoint accepts the payload specified by MediaMTX (classified in AuthPayload.cs)

--- SentryVision.HubApi/Controllers/AuthController.cs
+++ SentryVision.HubApi/Controllers/AuthController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -29,8 +30,42 @@ namespace SentryVision.HubApi.Controllers
         public async Task<List<Token>> Get()
         {
             // TODO: Add authentication to ensure only the server owner can access this endpoint
+            _logger.LogInformation("{Time}: Getting all tokens from database", DateTimeOffset.UtcNow);
             return await _dbInteractor.Tokens.ToListAsync();
         }
 
+        // POST: api/Auth
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] AuthPayload payload)
+        {
+            string token = payload.password;
+            string ip = payload.ip;
+            _logger.LogInformation("{Time}: Received authentication request from {ip}", DateTimeOffset.UtcNow, ip);
+            if (token == "")
+            {
+                _logger.LogError("{Time}: No token provided by {ip}", DateTimeOffset.UtcNow, ip);
+                return StatusCode(401);
+            }
+            else
+            {
+                Array tokenArray = await _dbInteractor.Tokens.ToArrayAsync();
+                foreach (Token t in tokenArray)
+                {
+                    if (t.AccessToken == token)
+                    {
+                        _logger.LogInformation("{Time}: Token provided by {ip} is valid", DateTimeOffset.UtcNow, ip);
+                        return Ok();
+                    }
+                    else
+                    {
+                        _logger.LogError("{Time}: Token provided by {ip} is invalid", DateTimeOffset.UtcNow, ip);
+                        return StatusCode(403);
+                    }
+                }
+
+            }
+            return StatusCode(500);
+        }
     }
 }
---
+++ SentryVision.HubApi/Models/AuthPayload.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Principal;
+
+namespace SentryVision.HubApi.Models;
+
+public class AuthPayload
+{
+    public string ip { get; set; }
+    public string user { get; set; }
+    public string password { get; set; }
+    public string path { get; set; }
+    public string protocol { get; set; }
+    public string id { get; set; }
+    public string action { get; set; }
+    public string query { get; set; }
+}
